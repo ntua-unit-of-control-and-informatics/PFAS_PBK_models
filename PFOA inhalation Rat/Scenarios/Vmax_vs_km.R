2fcf24a76b07eb9a4a81967f65547385713b992c
Periklis_ts <ptsirostsib@gmail.com>
2024-06-03 09:42:20 +0300

    Periklis Tests

---
+++ PFOA inhalation Rat/Scenarios/Vmax_vs_km.R
@@ -0,0 +1,24 @@
+VmK_Oatp = 53725.15
+VmK_Oat1 = 15019.93
+VmK_Oat3 = 21952.21
+VmK_Urat =  8780
+VmL_Oatp = 527951
+VmL_Oatp2 = 84756.11
+VmL_Ntcp = 170307
+VmIn_Oatp2 = 12000
+  
+KmK_Oatp = 52338.45
+KmK_Oat1 = 17887.82
+KmK_Urat = 339554
+KmL_Oatp2 = 61563.93
+KmL_Ntcp = 8281.4
+KmIn_Oatp2 = 3436
+
+VmK_Oatp/KmK_Oatp = 1
+VmK_Oat1/KmK_Oat1 = 0.839
+VmK_Urat/KmK_Urat = 0.06
+VmL_Oatp/KmK_Oatp = 10.08725
+VmL_Oatp2/KmL_Oatp2 = 0.726
+VmL_Ntcp/KmL_Ntcp = 20.565
+VmIn_Oatp2/KmIn_Oatp2 = 3.5
+
